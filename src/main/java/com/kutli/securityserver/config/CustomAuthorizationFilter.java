a238c0165fd8b06c1cb82b8c6281404536368ec8
kutli <kutlidev@gmail.com>
2022-09-27 15:26:35 -0500

    eureka properties

--- src/main/java/com/kutli/securityserver/config/CustomAuthorizationFilter.java
+++ src/main/java/com/kutli/securityserver/config/CustomAuthorizationFilter.java
@@ -42,7 +42,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
             if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 try {
                     String token = authorizationHeader.substring("Bearer ".length());
-                    Algorithm algorithm = Algorithm.HMAC256("jwt_secret".getBytes());
+                    Algorithm algorithm = Algorithm.HMAC256(jwt_secret.getBytes());
                     JWTVerifier verifier = JWT.require(algorithm).build();
                     DecodedJWT decodedJWT = verifier.verify(token);
                     String username = decodedJWT.getSubject();
